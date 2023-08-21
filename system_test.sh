18bf03b31f25d78e3d809031b08ac93ce61e72ea
trex2000 <trex_daemon@yahoo.com>
2023-08-21 17:24:17 +0300

    Update system_test.sh
    Switched to NUT

--- system_test.sh
+++ system_test.sh
@@ -19,21 +19,21 @@ echo -e "\e[36mPower:\e[0m"
 SENSORS=$(sudo /usr/bin/sensors | grep -i -A6 'ACPI interface')
 echo "$SENSORS"
 
-echo -e "\e[94mChecking UPS Status:\e[0m"
-echo  -n "STATUS: "
-apcaccess -p STATUS
-echo  -n "SELFTEST: "
-apcaccess -p SELFTEST
-echo  -n "Supply Voltage: "
-apcaccess -p LINEV
-echo -n "Load: "
-apcaccess -p LOADPCT
-echo  -n "Battery Charge: "
-apcaccess -p BCHARGE
-echo  -n "Available time with current load: "
-apcaccess -p TIMELEFT
-echo  -n "Battery Voltage: "
-apcaccess -p BATTV
+echo -e "\e[94mChecking UPS:\e[0m"
+echo  -e "\e[94mSTATUS: \e[0m"
+sudo upsc apc-700@localhost  2>&1 | grep ups.status
+echo  -e "\e[94mSELFTEST: \e[0m"
+sudo upsc apc-700@localhost  2>&1 | grep ups.test.result
+echo  -e "\e[94mSupply Voltage: \e[0m"
+sudo upsc apc-700@localhost  2>&1 | grep input.voltage
+echo -e "\e[94mLoad: \e[0m"
+sudo upsc apc-700@localhost  2>&1 | grep ups.load
+echo  -e "\e[94mBattery Charge: \e[0m"
+sudo upsc apc-700@localhost  2>&1 | grep battery.charge
+echo  -e "\e[94mAvailable time with current load: \e[0m"
+sudo upsc apc-700@localhost  2>&1 | grep battery.runtime
+echo  -e "\e[94mBattery Voltage: \e[0m"
+sudo upsc apc-700@localhost  2>&1 | grep battery.voltage
 
 
 
@@ -41,7 +41,7 @@ echo -e "\e[94mChecking Disk Status:\e[0m"
 for DEV in /sys/block/sd*
 do
  DEV=`basename $DEV`
- sdparm -i /dev/$DEV | grep -i $DEV
+ sudo sdparm -i /dev/$DEV | grep -i $DEV
  out=$(sudo /usr/sbin/smartctl -H /dev/sda -d sat)
  searchString="PASSED"
  if `echo ${out} | grep "${searchString}" 1>/dev/null 2>&1`
@@ -50,7 +50,10 @@ do
  else
  echo -e "Default \e[31mRedDrive has S.M.A.R.T. errors!\e[0"
  fi
+ sudo hdparm -C /dev/$DEV
 done
 echo -e "\e[94mRaid1 Array Status:\e[0m"
 sudo mdadm --detail /dev/md0  | grep -i State
+echo -e "\e[94mDocker Status:\e[0m"
+sudo docker stats --no-stream --all
 exit 0
