ec00b061afc2bbb7de45be4f070e75e8087d2dfd
trex2000 <trex_daemon@yahoo.com>
2024-02-09 12:26:22 +0200

    Update system_backup.sh
    Added both NUT and APCUPSD query scripts

--- system_backup.sh
+++ system_backup.sh
@@ -1,105 +1,81 @@
-#!/bin/sh
-if grep -qs '/mnt/500gb' /proc/mounts; then
-    echo "Drive is mounted."
+#! /bin/bash
+#USE_NUT_SERVER=TRUE
+
+echo -e "\e[94mUpdate Status:\e[0m"
+echo "$(lsb_release -d | grep -i Ubuntu)"
+/usr/lib/update-notifier/update-motd-updates-available
+
+echo -e "\e[94mSensorStatus:\e[0m"
+#echo -e "\e[36mFan Speed:\e[0m"
+#SENSORS=$(/usr/bin/sensors | grep -i fan)
+#echo "$SENSORS"
+
+echo -e "\e[36mTemp:\e[0m"
+SENSORS=$(sudo /usr/bin/sensors | grep -i temp.:)
+echo "$SENSORS"
+SENSORS=$(sudo /usr/bin/sensors | grep -i Core)
+echo "$SENSORS"
+
+echo -e "\e[36mPower:\e[0m"
+SENSORS=$(sudo /usr/bin/sensors | grep -i -A6 'ACPI interface')
+echo "$SENSORS"
+
+if [[ $USE_NUT_SERVER ]]; then
+    echo -e "\e[94mChecking UPS:\e[0m"
+    echo  -e "\e[94mSTATUS: \e[0m"
+    sudo upsc apc-700@localhost  2>&1 | grep ups.status
+    echo  -e "\e[94mSELFTEST: \e[0m"
+    sudo upsc apc-700@localhost  2>&1 | grep ups.test.result
+    echo  -e "\e[94mSupply Voltage: \e[0m"
+    sudo upsc apc-700@localhost  2>&1 | grep input.voltage
+    echo -e "\e[94mLoad: \e[0m"
+    sudo upsc apc-700@localhost  2>&1 | grep ups.load
+    echo  -e "\e[94mBattery Charge: \e[0m"
+    sudo upsc apc-700@localhost  2>&1 | grep battery.charge
+    echo  -e "\e[94mAvailable time with current load: \e[0m"
+    sudo upsc apc-700@localhost  2>&1 | grep battery.runtime
+    echo  -e "\e[94mBattery Voltage: \e[0m"
+    sudo upsc apc-700@localhost  2>&1 | grep battery.voltage
 else
-    echo "Google Drive not mounted. Mounting drive"
-    mount /media/gdrive
-    if [ $? -eq 0 ]; then
-       echo "Mount success!"
-    else
-       echo "Something went wrong with the mount..."
-    exit
-    fi
-fi
-#all ok, try to copy the stuff
-
-echo "Backup is starting"
-echo "Deleting old backups"
-
-rm -f /mnt/500gb/_backups/*.zip
-
-echo "zipping etc"
-filename="/mnt/500gb/_backups/etc_backup_"`date +"%d-%m-%Y"`".zip"
-zip -r -y $filename  /etc/*
-
-echo "zipping /var/bin"
-filename="/mnt/500gb/_backups/var_bin_backup_"`date +"%d-%m-%Y"`".zip"
-zip -r -y $filename  /var/bin/* --exclude=*Snapshots*
-
-echo "zipping /usr/local/bin"
-filename="/mnt/500gb/_backups/usr_local_bin_backup_"`date +"%d-%m-%Y"`".zip"
-zip -r -y $filename  /usr/local/bin/*
-
-echo "zipping /var/etc"
-filename="/mnt/500gb/_backups/var_etc_backup_"`date +"%d-%m-%Y"`".zip"
-zip -r -y $filename  /var/etc/*
-
-
-
-if [ -f /tmp/mysqldb.sql ];
-then
-   rm /tmp/mysqldb.sql
-else
-   echo "Old mydql db backup does not exists"
-fi
-mysqldump -uroot -ppassword --all-databases --skip-lock-tables > /tmp/mysqldb.sql
-echo "zipping mysql database"
-filename="/mnt/500gb/_backups/mysqldb_backup_"`date +"%d-%m-%Y"`".zip"
-zip -r -y $filename  /tmp/mysqldb.sql
-if [ -f /tmp/mysqldb.sql ];
-then
-   rm /tmp/mysqldb.sql
-else
-   echo "Old mydql db backup does not exists"
+    echo -e "\e[36mPower:\e[0m"
+    SENSORS=$(sudo /usr/bin/sensors | grep -i -A6 'ACPI interface')
+    echo "$SENSORS"
+    echo -e "\e[94mChecking UPS Status:\e[0m"
+    echo  -n "STATUS: "
+    apcaccess -p STATUS
+    echo  -n "SELFTEST: "
+    apcaccess -p SELFTEST
+    echo  -n "Supply Voltage: "
+    apcaccess -p LINEV
+    echo -n "Load: "
+    apcaccess -p LOADPCT
+    echo  -n "Battery Charge: "
+    apcaccess -p BCHARGE
+    echo  -n "Available time with current load: "
+    apcaccess -p TIMELEFT
+    echo  -n "Battery Voltage: "
+    apcaccess -p BATTV
 fi
 
-echo "zipping systemd"
-filename="/mnt/500gb/_backups/systemd_backup_"`date +"%d-%m-%Y"`".zip"
-zip -r -y $filename  /var/lib/systemd/*
 
 
-echo "zipping webpages"
-filename="/mnt/500gb/_backups/www_backup_"`date +"%d-%m-%Y"`".zip"
-zip -r -y $filename  /var/www/Webpages/*
-
-
-echo "zipping email"
-filename="/mnt/500gb/_backups/email_backup_"`date +"%d-%m-%Y"`".zip"
-zip -r -y $filename  /var/mail/*
-
-echo "zipping tor"
-filename="/mnt/500gb/_backups/tor_backup_"`date +"%d-%m-%Y"`".zip"
-zip -r -y $filename  /var/lib/tor/*
-
-echo "zipping mediatomb"
-filename="/mnt/500gb/_backups/mediatomb_backup_"`date +"%d-%m-%Y"`".zip"
-zip -r -y $filename  /var/lib/mediatomb/*
-
-
-echo "Zipping Virtualbox Config"
-filename="/mnt/500gb/_backups/Vbox_Config_backup_"`date +"%d-%m-%Y"`".zip"
-zip -r -y $filename  /root/.VirtualBox/*.xml 
-
-echo "Copy virutalbox hard drive"
-systemctl stop winserver
-filename="/mnt/500gb/_backups/Vbox_Winserver_hdd_"`date +"%d-%m-%Y"`".zip"
-zip -r -y  $filename  /root/VirtualBox\ VMs/Winserver/hdd/*.vdi*
-
-echo "Zipping Winserver Config"
-filename="/mnt/500gb/_backups/Vbox_Winserver_Config_backup_"`date +"%d-%m-%Y"`".zip"
-zip -r -y -0 $filename  /root/VirtualBox\ VMs/Winserver/*.vbox*
-
-
-echo "Copy ebooks"
-rsync -av -e  /mnt/500gb/_ebooks\ \&\ audiobooks/text\ books/ /mnt/WD/
-
-echo "Copy Nyakok"
-rsync -av -e  cd /mnt/500gb/_Nyakok/ /mnt/WD/_Nyakok
-
-echo "Copy Picture"
-rsync -av -e  --bwlimit=10000  /mnt/500gb/Picture/ /mnt/WD/Picture
-
-echo "Copy Pictures"
-rsync -av -e --bwlimit=10000 cd /mnt/500gb/Pictures/ /mnt/WD/Pictures
-
-echo "All done"
+echo -e "\e[94mChecking Disk Status:\e[0m"
+for DEV in /sys/block/sd*
+do
+ DEV=`basename $DEV`
+ sudo sdparm -i /dev/$DEV | grep -i $DEV
+ out=$(sudo /usr/sbin/smartctl -H /dev/sda -d sat)
+ searchString="PASSED"
+ if `echo ${out} | grep "${searchString}" 1>/dev/null 2>&1`
+ then
+  echo -e "Status \e[32mOk\e[0m"
+ else
+ echo -e "Default \e[31mRedDrive has S.M.A.R.T. errors!\e[0"
+ fi
+ sudo hdparm -C /dev/$DEV
+done
+echo -e "\e[94mRaid1 Array Status:\e[0m"
+sudo mdadm --detail /dev/md0  | grep -i State
+echo -e "\e[94mDocker Status:\e[0m"
+sudo docker stats --no-stream --all
+exit 0
